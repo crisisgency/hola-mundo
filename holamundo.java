fb45643360875ae99ecf53c54f5bbc9b34081c4e
crisisgency <crisisgency@gmail.com>
2015-03-29 01:48:39 -0300

    prueba de push y tag

--- holamundo.java
+++ holamundo.java
@@ -10,6 +10,12 @@ for(int k=0;i<10;k++){
          {System.out.println("enumerar:"+i);
          {System.out.println("enumerar:"+i);
          }}
+for(int k=0;i<10;k++){
+	for(int i=0;i<10;i++)
+	{System.out.println("enumerar:"+i);
+         {System.out.println("enumerar:"+i);
+         {System.out.println("enumerar:"+i);
+         }}
 
   }
 }
